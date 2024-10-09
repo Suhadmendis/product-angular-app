38a0a5b907842deeb58d2b278927b17555e78a0b
Suhad Mendis <Suhad.a.mendis@gmail.com>
2024-10-09 13:56:56 +0530

    fixes

---
+++ src/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,  // Set to true for production builds
+    apiUrl: 'https://localhost:7254/api/Product', // Your API URL
+    // Add other configurations if necessary
+
+};
